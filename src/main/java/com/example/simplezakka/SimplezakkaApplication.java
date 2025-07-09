162723e9465201053e7438a20d0dd66c90986cc7
yuya0743 <yuya.yoshioka@avantcorp.com>
2025-07-09 11:27:52 +0900

    最初のコミット

---
+++ src/main/java/com/example/simplezakka/SimplezakkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplezakka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplezakkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplezakkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simplezakka/SimplezakkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplezakka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplezakkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
