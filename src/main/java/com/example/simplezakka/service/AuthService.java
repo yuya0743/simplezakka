1faccfa6e6614edfbb9d50a3aa694a78fd66c561
etsu.miura <etsu.miura@avantcorp.com>
2025-07-14 19:35:12 +0900

    hnekou1

---
+++ src/main/java/com/example/simplezakka/service/AuthService.java
@@ -0,0 +1,64 @@
+package com.example.simplezakka.service;
+
+import com.example.simplezakka.dto.Login.LoginInfo;
+import com.example.simplezakka.dto.Login.UserInfo;
+import com.example.simplezakka.dto.product.ProductDetail;
+import com.example.simplezakka.entity.Product;
+import com.example.simplezakka.entity.User1;
+import com.example.simplezakka.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import jakarta.persistence.criteria.CriteriaBuilder.In;
+import jakarta.servlet.http.HttpSession;
+import java.util.Optional;
+
+@Service
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private User1 authenticatedUser;
+
+    @Autowired
+    public AuthService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+        this.authenticatedUser = null;
+    }
+
+    public User1 getAuthenticatedUser() {
+        return authenticatedUser;
+    }
+
+    public boolean login(String email, String password) {
+        Optional<User1> userOptional = userRepository.findById(email);  
+
+        if (userOptional.isPresent()) {
+            User1 user = userOptional.get();
+            if (user.getPassword().equals(password)) {  
+                this.authenticatedUser = user;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isAuthenticated() {
+        return this.authenticatedUser != null;
+    }
+
+    public void logout() {
+        this.authenticatedUser = null;
+    }
+
+     public LoginInfo finduserByEmail(String email) {
+        Optional<User1> userOpt = userRepository.findById(email);
+        return userOpt.map(this::convertToLogin).orElse(null);
+    }
+
+    public LoginInfo convertToLogin(User1 user) {
+        return new LoginInfo(
+                user.getEmail(),
+                user.getPassword()  
+        );
+    }
+}
