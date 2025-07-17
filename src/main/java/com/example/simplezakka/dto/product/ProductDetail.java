3a5d9aa27ea8f458c45177961021dbc79f24703b
Hikari0430 <hikari.nakamura@avantcorp.com>
2025-07-17 18:04:32 +0900

    絶対にマージするなよ

--- src/main/java/com/example/simplezakka/dto/product/ProductDetail.java
+++ src/main/java/com/example/simplezakka/dto/product/ProductDetail.java
@@ -1,9 +1,10 @@
 package com.example.simplezakka.dto.product;
 
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.Max;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,18 +18,19 @@ public class ProductDetail {
     @NotBlank(message = "商品名は必須です")
     private String name;
 
+    @Size(max = 300, message = "説明文は300字以内でなければなりません")  // 300字以内
+    private String description;
+
     @NotNull(message = "価格は必須です")
     @Min(value = 1, message = "価格は1以上でなければなりません")  // ★ここで正の数を制限
     private Integer price;
-
-    private String imageUrl;
-    private String category; 
-    private String material;
 
     @NotNull(message = "在庫数は必須です")
     @Min(value = 0, message = "在庫数は0以上でなければなりません")  // 0はOK、負数はNG
     private Integer stock;
 
-    @Max(value = 300, message = "説明文は300字以内でなければなりません")  // 300字以内
-    private String description;
+    private String imageUrl;
+    private Boolean isRecommended;
+    private String category; 
+    private String material;
 }
